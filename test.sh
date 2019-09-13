c6b27fd8a4d704ac1789fc6b5063fd83636cfc84
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2019-09-13 09:57:18 +0900

    wewg

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@
 vagrant(https://www.vagrantup.com/)とvirtualbox(https://www.virtualbox.org/)はあらかじめ入れといてね
 -----------------------------------------------------------------------
 たった３回コマンドを打つだけでおけ！
-$curl -O https://raw.githubusercontent.com/hibiking-0422/rails_install.sh/master/vagrant.bat
-$vagrant.bat
+$curl -O https://raw.githubusercontent.com/hibiking-0422/centos7_test/master/centos7_test.bat
+$centos7_test.bat
 $bash <(curl -s https://raw.githubusercontent.com/hibiking-0422/rails_install.sh/master/rails6.0_install.sh)
 -----------------------------------------------------------------------
 commentout
